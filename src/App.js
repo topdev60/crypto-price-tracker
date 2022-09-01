ee5528381c91d73e8b3f4c59f24d73e57db8c6a5
topdev60 <topdev60@gmail.com>
2022-09-01 15:10:15 +0200

    crypto-price-tracker

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./styles/global.scss";
+import CoinTable from "./pages/CoinTable";
+import { Routes, Route } from "react-router-dom";
+import EachCoin from "./components/EachCoin";
+import { useSelector } from "react-redux";
+import Exchange from "./pages/Exchange";
+import CoinCategories from "./pages/CoinCategories";
+
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
+  // const [coin, setCoin] = useState({});
+
+  // const eachCoinId = useSelector((state) => state.eachCoin);
+  // console.log(eachCoinId);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  // useEffect(() => {
+  //   axios.get(`https://api.coingecko.com/api/v3/coins/${eachCoinId}`)
+  //   .then(res => {
+  //     setCoin(res.data);
+  //     setLoading(false);
+  //   })
+  //   .catch(error => console.log(error))
+  // }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div className="coin-app">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            !loading ? (
+              <CoinTable
+                filteredCoins={filteredCoins}
+                handleChange={handleChange}
+                setLoading={setLoading}
+              />
+            ) : (
+              <p>Loading...</p>
+            )
+          }
+        />
+        <Route
+          path="/exchange"
+          element={<Exchange handleChange={handleChange} />}
+        />
+        <Route
+          path="/categories"
+          element={<CoinCategories handleChange={handleChange} />}
+        />
+        <Route
+          path="/:ID"
+          element={!loading ? <EachCoin setLoading={setLoading} /> : <p>Loading...</p>}
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Logo from "../images/coinize2.svg";
+import { NavLink, useLocation } from "react-router-dom";
+
+const Navbar = ({ handleChange }) => {
+  let activeClassName = "nav-active";
+  const location = useLocation();
+  const isActive = location.pathname;
+
+  return (
+    <nav className="coin-search">
+      <div className="coin-logo-container">
+        <NavLink to="/">
+          <div className="coin-image-container">
+            <img className="coin-logo" src={Logo} alt="logo" />
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div
+            className={`coin-pages-container ${
+              isActive === "/" ? activeClassName : undefined
+            }`}
+          >
+            <h3>Market</h3>
+          </div>
+        </NavLink>
+        <NavLink to="/exchange">
+          <div
+            className={`coin-pages-container ${
+              isActive === "/exchange" ? activeClassName : undefined
+            }`}
+          >
+            <h3>Exchange</h3>
+          </div>
+        </NavLink>
+        <NavLink to="/categories">
+          <div
+            className={`coin-pages-container ${
+              isActive === "/categories" ? activeClassName : undefined
+            }`}
+          >
+            <h3>Categories</h3>
+          </div>
+        </NavLink>
+      </div>
+
+      <form className="coin-input-container">
+        <input
+          type="text"
+          placeholder="Search a currency"
+          className="coin-input"
+          onChange={handleChange}
+        />
+      </form>
+    </nav>
+  );
+};
+
+export default Navbar;
